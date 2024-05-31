b27397c0bf8fba38fdf8efc8b1bb09cf0158fccf
bih9724man <152031575+bih9724man@users.noreply.github.com>
2024-05-31 09:24:33 +0100

    Add files via upload

---
+++ Linked_List_Cycle.c
@@ -0,0 +1,14 @@
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *slow=head;
+    struct ListNode *fast=head;
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast) return 1;
+    }
+    return 0;
+}
